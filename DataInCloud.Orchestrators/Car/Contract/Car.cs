5961756ae43891a5e8100a49137469acf6897055
kpi-anton-akh <127939194+kpi-anton-akh@users.noreply.github.com>
2024-03-20 11:07:39 +0200

    Phase1: Implement CRUD for car resource (#1)
    * Implement CRUD for car resource
    
    * Extand test coverage
    
    ---------
    
    Co-authored-by: Anton Akhaladze <80323512+anton-akh@users.noreply.github.com>

---
+++ DataInCloud.Orchestrators/Car/Contract/Car.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataInCloud.Orchestrators.Car.Contract;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DoorsCount { get; set; }
+    public bool IsBuyEnable { get; set; }
+}
